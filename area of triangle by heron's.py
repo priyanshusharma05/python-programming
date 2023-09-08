3a647188f1f44d49fb8d0cf5987608aeb2e2ce77
priyanshusharma05 <142593082+priyanshusharma05@users.noreply.github.com>
2023-09-09 00:12:57 +0530

    Add files via upload

---
+++ area of triangle by heron's.py
@@ -0,0 +1,7 @@
+a=eval(input("enter the first side"))
+b=eval(input("enter the second side"))
+c=eval(input("enter the third  side"))
+s=(a+b+c)/2
+
+ar= (s*(s-a)*(s-b)*(s-c))**.5
+print("area of triangle is =",ar)
