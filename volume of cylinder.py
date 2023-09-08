52e01b7698e481d574fa3df7a4b5d6460ddc8829
priyanshusharma05 <142593082+priyanshusharma05@users.noreply.github.com>
2023-09-09 00:36:59 +0530

    Add files via upload

---
+++ volume of cylinder.py
@@ -0,0 +1,4 @@
+r=eval(input("enter the radius of cylinder"))
+h=eval(input("enter the height of cylinder"))
+vol=3.14*(r)**2*h
+print("volume of cylinder is=",vol)
---
+++ volume of sphere.py
@@ -0,0 +1,3 @@
+r=eval(input("enter the radius of circle"))
+v=4/3*3.14*(r)**3
+print("volume of sphere is=",v)
